9d562c48203128e15802c146f0eb5064c605c135
Nick Ball <nick@npbee.me>
2023-09-24 14:24:24 -0700

    Focus rings

--- src/routes/(app)/tracks/[trackId]/+page.ts
+++ src/routes/(app)/tracks/[trackId]/+page.ts
@@ -4,8 +4,6 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async function load({ params }) {
   const track = tracks[params.trackId];
 
-  console.log('load: ', track);
-
   return {
     track,
   };
